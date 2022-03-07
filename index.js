543f7350af18878442f8bc46a2e0344ac6f426a3
Taulant Ismailgeci <+>
2022-03-07 05:22:46 -0500

    commit tree 5f1af59ae5d639e8ed8f6db2f28c6ac76eeec313

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    //console.log(`Hi, my name is ${name}.`);
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language) {
+    //console.log(`Hello,`);
+}
+
+function introductionWithLanguageOptional(name, language){
+
+}
+introduction("Aki");
+introduction("Samip");
